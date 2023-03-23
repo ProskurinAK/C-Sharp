07090d51afe305c2f0bc3fab9a1b84aa055bb9a5
ProskurinAK <andrew.prosto.andrew@gmail.com>
2023-03-23 12:39:08 +0300

    Add UploadImages project

---
+++ WindowsForms/UploadImages/UploadImages/MainWindow.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace UploadImages
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13F);
+            this.label1.Location = new System.Drawing.Point(34, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Изображения";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F);
+            this.button1.Location = new System.Drawing.Point(632, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(39, 165);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(875, 330);
+            this.panel1.TabIndex = 2;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(960, 523);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ WindowsForms/UploadImages/UploadImages/MainWindow.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploadImages
+{
+    public partial class MainWindow : Form
+    {
+        List<PictureBox> ListOfPictureBox = new List<PictureBox>(); // Список всех созданных объектов PictureBox
+        PictureBox SelectedItem;    // Выбранный наведением курсора мыши объект PictureBox
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Функция создания контекстного меню
+        /// </summary>
+        /// <returns></returns>
+        private ContextMenuStrip CreateContextMenu()
+        {
+            ContextMenuStrip MyContextMenuStrip = new ContextMenuStrip();
+
+            ToolStripMenuItem Increase = new ToolStripMenuItem("Увеличить изображение");
+            ToolStripMenuItem Delete = new ToolStripMenuItem("Удалить изображение");
+
+            MyContextMenuStrip.Items.AddRange(new[] { Increase, Delete });
+
+            Increase.Click += new EventHandler(this.IncreaseMenuItem_Click);
+            Delete.Click += new EventHandler(this.DeleteMenuItem_Click);
+
+            return MyContextMenuStrip;
+        }
+        /// <summary>
+        /// Событие увеличения изображения по щелчку кнопки из контекстного меню
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void IncreaseMenuItem_Click(object sender, EventArgs e)
+        {
+            PictureBox LargePictureBox = new PictureBox();
+            LargePictureBox.Image = SelectedItem.Image;
+            LargePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            Form LargeForm = new Form();
+
+            LargeForm.StartPosition = FormStartPosition.CenterScreen;
+            LargeForm.Controls.Add(LargePictureBox);
+            LargeForm.Size = new Size(700, 500);
+
+            LargePictureBox.Dock = DockStyle.Fill;
+
+            LargeForm.Show();
+        }
+        /// <summary>
+        /// Событие удаления изображения по щелчку кнопки из контекстного меню
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteMenuItem_Click(object sender, EventArgs e)
+        {
+            Point DisplacementPoint = new Point(0, 0);
+            bool Flag = false;
+
+            for (int i = 0; i < ListOfPictureBox.Count; i++)
+            {
+                if (ListOfPictureBox[i] == SelectedItem)
+                {
+                    DisplacementPoint = SelectedItem.Location;
+                    panel1.Controls.Remove(SelectedItem);
+                    ListOfPictureBox.Remove(SelectedItem);
+                    Flag = true;
+                }
+                if (Flag == true && i != ListOfPictureBox.Count)
+                {
+                    Point Tmp = ListOfPictureBox[i].Location;
+                    ListOfPictureBox[i].Location = DisplacementPoint;
+                    DisplacementPoint = Tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// Событие выбора объекта PictureBox наведением курсора мыши
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PictureBox_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is PictureBox)
+            {
+                SelectedItem = (PictureBox)sender;
+            }
+        }
+        /// <summary>
+        /// Событие добавления нового изображения по щелчку кнопки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PictureBox NewPictureBox = new PictureBox();
+
+            // Алгоритм добавления нового PictureBox
+            if (ListOfPictureBox.Count == 0)
+            {
+                NewPictureBox.Height = panel1.Height - 20;
+                NewPictureBox.Width = Convert.ToInt32(panel1.Width / 2.5);
+                NewPictureBox.Location = new Point(0, 0);
+                NewPictureBox.ContextMenuStrip = CreateContextMenu();
+                NewPictureBox.MouseEnter += new EventHandler(this.PictureBox_MouseEnter);
+            }
+            else
+            {
+                NewPictureBox.Height = panel1.Height - 20;
+                NewPictureBox.Width = Convert.ToInt32(panel1.Width / 2.5);
+                NewPictureBox.Location = new Point(ListOfPictureBox[ListOfPictureBox.Count - 1].Location.X + ListOfPictureBox[ListOfPictureBox.Count - 1].Width + 50,
+                    ListOfPictureBox[ListOfPictureBox.Count - 1].Location.Y);
+                NewPictureBox.ContextMenuStrip = CreateContextMenu();
+                NewPictureBox.MouseEnter += new EventHandler(this.PictureBox_MouseEnter);
+            }
+
+            ListOfPictureBox.Add(NewPictureBox);
+
+            panel1.Controls.Add(NewPictureBox);
+
+            // Открытие файла с изображением из проводника
+            OpenFileDialog FileDialog = new OpenFileDialog();
+
+            NewPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                NewPictureBox.Image = Image.FromFile(FileDialog.FileName);
+            }
+
+            // Условие отмены создания PictureBox
+            if (NewPictureBox.Image == null)
+            {
+                ListOfPictureBox.Remove(NewPictureBox);
+                panel1.Controls.Remove(NewPictureBox);
+            }
+        }
+    }
+}
